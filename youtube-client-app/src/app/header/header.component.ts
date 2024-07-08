bc9070208ea96384d2b9dcf4250ccefc31637d4b
ARTQ\ChikunovS <ChikunovS@quantumart.ru>
2024-07-08 19:08:58 +0300

    init: header component

--- youtube-client-app/src/app/header/header.component.ts
+++ youtube-client-app/src/app/header/header.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzAnchorModule } from 'ng-zorro-antd/anchor';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NzButtonModule,
+    NzIconModule,
+    NzInputModule,
+    FormsModule,
+    NzTypographyModule,
+    NzAnchorModule,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  public searchValue: string = '';
+  public sorting: string = 'none';
+  public isShow: boolean = false;
 
+  public toggleSettingShow(): void {
+    this.isShow = !this.isShow;
+  }
+
+  public setSorting(type: string): void {
+    this.sorting = type;
+  }
 }
