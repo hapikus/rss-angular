c7047fdc03907837fdf5697494099587f455db52
ARTQ\ChikunovS <ChikunovS@quantumart.ru>
2024-07-29 12:41:50 +0300

    feat: add formArray, reset button

--- youtube-client-app/src/app/pages/admin/admin.component.ts
+++ youtube-client-app/src/app/pages/admin/admin.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { FormBuilder, FormControl, FormRecord, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { CommonModule } from '@angular/common';
@@ -27,16 +27,16 @@ import { ErrorsFormatterPipe } from '../../shared/pipes/errors-formatter.pipe';
 })
 export class AdminComponent {
   public MAX_TAGS = 5;
+  public formArray = new FormArray<FormControl<string | null>>([]);
+
   public createCardForm = this.fb.group({
     title: ['', [Validators.required, titleValidator()]],
     description: ['', descriptionValidator()],
     previewImage: ['', [Validators.required, previewImageValidator()]],
     video: ['', [Validators.required, videoValidator()]],
     createDate: this.fb.control<Date | null>(null, [Validators.required]),
+    tags: this.formArray,
   });
-
-  public tags: Array<{ id: number; controlInstance: string }> = [];
-  public validateTags: FormRecord<FormControl<string | null>> = this.fb.record({});
 
   public disabledDates(date: Date) {
     return date.getTime() > new Date().getTime();
@@ -44,33 +44,25 @@ export class AdminComponent {
 
   public addTag(e?: MouseEvent): void {
     e?.preventDefault();
-
-    const id = (this.tags.at(-1)?.id ?? -1) + 1;
-    const control = {
-      id,
-      controlInstance: `tag${id}`,
-    };
-    const index = this.tags.push(control);
-    this.validateTags.addControl(
-      this.tags[index - 1].controlInstance,
-      this.fb.control('', Validators.required),
-    );
+    const newControl = new FormControl('', [Validators.required]);
+    this.formArray.push(newControl);
   }
 
-  public removeTag(i: { id: number; controlInstance: string }, e: MouseEvent): void {
+  public removeTag(i: number, e: MouseEvent): void {
     e.preventDefault();
-    const index = this.tags.indexOf(i);
-    this.tags.splice(index, 1);
-    this.validateTags.removeControl(i.controlInstance);
+    this.formArray.removeAt(i);
   }
 
   public trackByFn(index: number): string {
     return index.toString();
   }
 
+  public reset() {
+    this.createCardForm.reset();
+  }
+
   public submit() {
     this.createCardForm.reset();
-    this.validateTags.reset();
   }
 
   constructor(private fb: FormBuilder) {}
